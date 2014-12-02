7929afa0eae7c27ae3b85d81135ab3c14d61e79f
Matt Cloutier <mgclouti@umich.edu>
2014-12-02 16:07:09 -0500

    Update inl.cpp

--- p4_blank/inl.cpp
+++ p4_blank/inl.cpp
@@ -17,9 +17,95 @@ Status Operators::INL(const string& result,           // Name of the output rela
                       const int reclen)               // Length of a tuple in the output relation
 {
   cout << "Algorithm: Indexed NL Join" << endl;
+  
+  //BASIC ALGORITHM PSEUDOCODE:
+  //foreach tuple r in R do
+  //    foreach tuple s in S do
+  //        if r1 == s1 then add <r,s> to result 
+  
+  //Declare and initialize 2 scans (one for the left attribute, and the other for the right)
+  
+  Status scanSelectStatus;
+  
+          
+  HeapFileScan myHeapScan = HeapFileScan(attrDesc1->relName, scanSelectStatus);
+  
+  if(scanSelectStatus != OK)
+      return scanSelectStatus;
+          
+  HeapFileScan myHeapScan2 = HeapFileScan(attrDesc2->relName, attrDesc2->attrOffset, attrDesc2->attrLen, (Datatype)attrDesc2->attrType, (char*)(attrDesc1.attrName), op, scanSelectStatus);
+  //ABOVE: (char*)(attrDesc1.attrName) is probably wrong.  Maybe not tho...
+  
+  if(scanSelectStatus != OK)
+      return scanSelectStatus;
+  
+  HeapFile myHeap(result, scanSelectStatus);
+  
+  RID myRid;
+  RID myRid2;
+  
+  Record newRecord;
+  newRecord.length = reclen;
+  newRecord.data = malloc (reclen);
+  Record oldRecord;
+  Record oldRecord2;
+  
+  while(myHeapScan.scanNext(myRid, oldRecord) == OK)
+  {
+      int offset = 0;
+      
+      if(scanSelectStatus != OK)
+      {
+        free(newRecord.data);
+        return scanSelectStatus;
+      }
+      
+      while(myHeapScan2.scanNext(myRid2, oldRecord2) == OK)
+      {
+            int offset2 = 0;      
 
+            if(scanSelectStatus2 != OK)
+            {
+              free(newRecord.data);
+              return scanSelectStatus;
+            }
+
+            for(int i = 0; i < projCnt; i++)
+            {
+              memcpy(((char *)newRecord.data) + offset, (char*)(oldRecord2.data) + attrDescArray[i].attrOffset, attrDescArray[i].attrLen);
+              offset += attrDescArray[i].attrLen;
+            }
+
+            scanSelectStatus = myHeap.insertRecord(newRecord, myRid);
+
+            if(scanSelectStatus != OK)
+            {
+                free(newRecord.data);
+                return scanSelectStatus;
+            }
+      }
+      
+      for(int i = 0; i < projCnt; i++)
+      {
+          memcpy(((char *)newRecord.data) + offset, (char*)(oldRecord.data) + attrDescArray[i].attrOffset, attrDescArray[i].attrLen);
+          offset += attrDescArray[i].attrLen;
+      }
+      
+      scanSelectStatus = myHeap.insertRecord(newRecord, myRid);
+      
+      if(scanSelectStatus != OK)
+      {
+        free(newRecord.data);
+        return scanSelectStatus;
+      }
+      
+      return OK;
+  }
+  
+  myHeapScan->endScan();
+  free(newRecord.data);
+  
   /* Your solution goes here */
-
+  delete myHeapScan;
   return OK;
 }
-
